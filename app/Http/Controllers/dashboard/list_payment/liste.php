6f3caced245e2661a6797617a9885c5313028715
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2023-11-21 12:07:59 +0100

    add

--- app/Http/Controllers/dashboard/list_payment/liste.php
+++ app/Http/Controllers/dashboard/list_payment/liste.php
@@ -12,18 +12,19 @@ use App\Models\fedapay as feda;
 
 class liste extends Controller
 {
-   
-    public function test(Request $request){
+
+    public function test(Request $request)
+    {
         $firstName = $request->firstName;
         $lastName = $request->lastName;
         $email = $request->email;
         $phone = $request->phone;
         $montant = $request->montant;
-        $devis_id =$request->devis_id;
+        $devis_id = $request->devis_id;
 
-    FedaPay::setEnvironment(env('FEDAPAY_Environment'));
-    FedaPay::setApiKey(env('FEDAPAY_PRIVATE_KEY'));
-        
+        FedaPay::setEnvironment(env('FEDAPAY_Environment'));
+        FedaPay::setApiKey(env('FEDAPAY_PRIVATE_KEY'));
+
         $transaction = Transaction::create(array(
             "description" => "Transaction for john.doe@example.com",
             "amount" => $montant,
@@ -38,50 +39,68 @@ class liste extends Controller
                     "country" => "bj"
                 ]
             ]
-          ));
-          $token = $transaction->generateToken();
-        $this->fedapayValidatePay($transaction->id,$token->url,$devis_id);
-     return redirect()->to($token->url);
-   }
-
-   
-private function fedapayValidatePay($fedapaytransactionId,$url,$devis_id)
-    {
-        $pay = new feda();
-        $pay->fedapayTransactionId = $fedapaytransactionId;
-        $pay->fedapayTransactionUrl = $url;
-        $pay->devis()->associate($devis_id);
-        $pay->save();
-
-        return $pay;
+        ));
+        $token = $transaction->generateToken();
+        $this->fedapayValidatePay($transaction->id, $token->url, $devis_id);
+        return redirect()->to($token->url);
     }
 
-   public function view($id){
-    Fedapay::setEnvironment(env('FEDAPAY_Environment'));
-    Fedapay::setApiKey(env('FEDAPAY_PRIVATE_KEY'));
-        
-    /**
-     * @var FedaPayObject
-     */
-    $response = Transaction::search($id);
-    $transactions = $response->transactions;
-    $meta = $response->meta;
-    // return view('dashboard.list_payment.liste');
-   }
 
-   public function confirm(Request $request){
-    $status = $request->status;
-    $close = $request->close;
-    $id = $request->id;
+    private function fedapayValidatePay($fedapaytransactionId, $url, $devis_id)
+    {
+        if (!$this->IdfedapayExists($devis_id)) {
+            $pay = new feda();
+            $pay->fedapayTransactionId = $fedapaytransactionId;
+            $pay->fedapayTransactionUrl = $url;
+            $pay->devis()->associate($devis_id);
+            $pay->save();
 
-    if ($status == 'pending') {
-        $status = 'en attente';
-        feda::where('fedapayTransactionId', $id)->update(['statut' => $status]);
-    } /* else {
+            return $pay;
+        } else {
+
+            $pay = feda::where('devis_id', $devis_id)->delete();
+            $pay = new feda();
+            $pay->fedapayTransactionId = $fedapaytransactionId;
+            $pay->fedapayTransactionUrl = $url;
+            $pay->devis()->associate($devis_id);
+            $pay->save();
+
+            return $pay;
+        }
+    }
+    private function IdfedapayExists($id)
+    {
+        return feda::where('devis_id', $id)->exists();
+    }
+
+    public function view($id)
+    {
+        Fedapay::setEnvironment(env('FEDAPAY_Environment'));
+        Fedapay::setApiKey(env('FEDAPAY_PRIVATE_KEY'));
+
+        /**
+         * @var FedaPayObject
+         */
+        $response = Transaction::search($id);
+        $transactions = $response->transactions;
+        $meta = $response->meta;
+        // return view('dashboard.list_payment.liste');
+    }
+
+    public function confirm(Request $request)
+    {
+        $status = $request->status;
+        $close = $request->close;
+        $id = $request->id;
+
+        if ($status == 'pending') {
+            $status = 'en attente';
+            feda::where('fedapayTransactionId', $id)->update(['statut' => $status]);
+        } /* else {
         # code...
     } */
-    
 
-     return redirect()->route('dashboard.home');
-   }
+
+        return redirect()->route('dashboard.home');
+    }
 }

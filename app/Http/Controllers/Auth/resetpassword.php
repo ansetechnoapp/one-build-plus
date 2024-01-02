4dc662ddf1468df498d5d25b8ad0557f5da42a1f
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2024-01-02 10:31:10 +0100

    save new password modif 4

--- app/Http/Controllers/Auth/resetpassword.php
+++ app/Http/Controllers/Auth/resetpassword.php
@@ -76,70 +76,7 @@ class resetpassword extends Controller
             
         } else {
                     echo "Entrer un Email correcte et verifier que tous les champs soit remplir ";
-                } 
-
-        //////////////////////////////
-        /* if (isset($newPassword) || isset($oldPasswordcurrent)) {
-            if ($newPassword == '' && $oldPasswordcurrent == '') {
-                dd('rrr');
-                echo " S'il vous plaît, remplissez tous les champs.";
-            } else {
-
-                // Définition des règles de validation
-                $rules = [
-                    'password' => ['required', 'string', 'max:255', 'min:5', 'confirmed'],
-                    /* 'password' => ['required', 'string', 'max:255', Password::min(8)
-                            ->mixedCase()
-                            ->numbers()
-                            ->symbols()
-                            ->uncompromised()], 
-                ];
-
-                // Définition des messages d'erreur personnalisés
-                $messages = [
-                    'password.min' => 'Le mot de passe doit contenir au moins 8 caractères.',
-                ];
-
-                // Définition des noms de champs personnalisés
-                $customAttributes = [
-                    'min' => 'mot de passe',
-                ];
-                // Validation des données envoyées dans la requête
-                try {
-                    $credentials = $request->validate($rules, $messages, $customAttributes);
-                    if (User::where('email', $request->email)->first() === null) {
-                        $forerror = [
-                            'parm1' => 'Tu es ici pourquoi ?',
-                        ];
-                        return redirect()->route('auth-re-password', $forerror);
-                    } else {
-
-                        if ($credentials) {
-                            User::where('email', $request->email)->first()->update([
-                                'password' => hash::make($newPassword),
-                            ]);
-                            return view('page_confirm_message.confirme_updateforgetpassword');
-                        } else {
-                            // L'authentification a échoué, redirigez l'utilisateur vers la page de connexion avec un message d'erreur
-                            return back()->withErrors([
-                                'email' => 'Les identifiants fournis ne correspondent pas à nos enregistrements.',
-                            ])->withInput($request->only('email'));
-                        }
-                    }
-                } catch (ValidationException $e) {
-                    // Gestion de l'exception ValidationException ici (par exemple, affichage des messages d'erreur)
-                    // Récupération des messages d'erreur de validation
-                    $errors = $e->validator->errors();
-
-                    // Redirection vers la page de formulaire avec les messages d'erreur
-                    return redirect()
-                        ->back()
-                        ->withErrors($errors);
                 }
-            }
-        } else {
-            echo "Entrer un Email correcte et verifier que tous les champs soit remplir ";
-        } */
     }
     public function authrepassword(Request $request)
     {

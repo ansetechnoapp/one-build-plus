bd6112c776a19b3b5f34abecd455234e5b3da0be
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2024-02-05 01:25:51 +0100

    test deploy29 2/4/2024 01:25

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Variables
-REMOTE_HOST="89.117.9.183"
-REMOTE_PORT="65002"
-REMOTE_USER="u212330119"
-REMOTE_PASSWORD="U6q^Xf$gNjHjq*rmZ"
-REMOTE_DIR="/obp/OBP/"
+# Informations de connexion SSH
+HOST="89.117.9.183"
+PORT="65002"
+USERNAME="u212330119"
+PASSWORD="kdkkqqdjq"
 
-# Command to connect via SSH and run 'composer install'
-sshpass -p "$REMOTE_PASSWORD" ssh -p "$REMOTE_PORT" "$REMOTE_USER"@"$REMOTE_HOST" << EOF
-  cd "$REMOTE_DIR"
-  composer install
+# Commandes pour se connecter via SSH
+sshpass -p $PASSWORD ssh -T -p $PORT $USERNAME@$HOST << EOF
+
+# Se déplacer dans le répertoire d'installation de Composer
+cd domains
+cd txbest.online/public_html/obp/
+
+# Afficher le répertoire actuel
+pwd
+
+# Créer le dossier 'hi'
+mkdir hi
+
+# Se déplacer dans le dossier 'hi'
+cd hi
+
+# Installer les dépendances avec Composer
+composer2 install
+
+# Quitter la connexion SSH
+exit
 EOF
